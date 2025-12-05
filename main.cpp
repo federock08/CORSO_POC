04f41d158c51f23addd294f9c21a9df11606a2fe
Federico <bianchini.federico@studenti-ittfedifermi.edu.it>
2025-12-05 15:25:32 +0100

    Creata la funzione menu

--- main.cpp
+++ main.cpp
@@ -5,5 +5,13 @@ using namespace std;
 // Questo codice è a scopo ditadittico, potrebbe non funzionare
 // Programma per Informatica
 int main() {
-	cout << "Hello World!" << endl;
+	cout << menu << endl;
+}
+
+// Funzione che fa vedere il menu
+void menu() {
+	cout << "====================" << endl;
+	cout << "        Menu        " << endl;
+	cout << "1) Opzione 1" << endl;
+	cout << "2) Opzione 2" << endl;
 }
